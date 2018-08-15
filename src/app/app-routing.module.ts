166830c55de7610424ae2e77f54968a16c8bf0a1
Will <doggy.huang@gmail.com>
2018-08-15 14:00:12 +0800

    示範 useHash 與 routerLinkActive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,15 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  {
-    path: 'page1',
-    component: Page1Component
-  },
-  {
-    path: 'page2',
-    component: Page2Component
-  },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false,
+    useHash: true
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
