0301ea55aae7abea5ee966556671b64b6bb4d2aa
Will <doggy.huang@gmail.com>
2018-08-15 15:31:46 +0800

    完成 /charts/flot 與 /charts/morris 基本套版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,31 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsComponent } from './forms/forms.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { FlotComponent } from './charts/flot/flot.component';
+import { MorrisComponent } from './charts/morris/morris.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'forms', component: FormsComponent },
+  {
+    path: 'charts',
+    children: [
+      { path: '', redirectTo: 'flot', pathMatch: 'full' },
+      { path: 'flot', component: FlotComponent },
+      { path: 'morris', component: MorrisComponent },
+    ]
+  },
   { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    enableTracing: false,
-    useHash: true
-  })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false,
+      useHash: true
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
