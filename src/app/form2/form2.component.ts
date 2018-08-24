99569c79639c8024673a7d214bc2e4959b2ef195
Will <doggy.huang@gmail.com>
2018-08-24 16:17:51 +0800

    示範 Reactive Forms 基本開發方法

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form2',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Form2Component implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      name: ['Will', [Validators.required, Validators.minLength(2)]],
+      tel: ['0988-888-888', [Validators.pattern(/\d{4}-\d{3}-\d{3}/)]]
+    });
+
+  }
+
+  doSubmit() {
+    console.log(this.form.value);
+    if (this.form.invalid) {
+      alert('表單因為還有問題，所以尚未送出！');
+    }
+    // TODO: Submit form!
+  }
 
   ngOnInit() {
   }
