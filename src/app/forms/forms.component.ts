6d5a3799d27a38744dc7182668cbc283e9d793e2
Will <doggy.huang@gmail.com>
2018-08-24 14:35:31 +0800

    示範 NgForm 的寫法與注意事項

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -11,11 +12,20 @@ export class FormsComponent implements OnInit {
   type: string;
 
   data: any = {
-    name: 'Will'
+    name: 'Will',
+    habbit: []
   };
 
   constructor(private router: Router,
               private route: ActivatedRoute) { }
+
+  doSubmit(form: NgForm) {
+    console.log(form.value);
+    if (form.invalid) {
+      alert('表單因為還有問題，所以尚未送出！');
+    }
+    // TODO: Submit form!
+  }
 
   ngOnInit() {
     // this.type = this.route.snapshot.params["type"];
